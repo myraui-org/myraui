efb8f59d8e76483eec338ce3e907644ba6746ce8
Moses Gitau <gitaumoses4@gmail.com>
2024-10-29 07:54:41 +0300

    feat(effect): configure effect to projects

--- packages/shared-utils/src/index.ts
+++ packages/shared-utils/src/index.ts
@@ -2,6 +2,4 @@ export * from './types';
 export * from './functions';
 export * from './objects';
 export * from './assertion';
-export * from './clsx';
-export * from './errors';
 export * from './strings';
